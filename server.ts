83f29b41a1617a4476d759a31b6591217005cdae
kwb020312 <kwb020312@naver.com>
2020-12-23 14:05:25 +0900

    서버 생성 완료 및 타입정의

--- server.ts
+++ server.ts
@@ -1,6 +1,37 @@
-import { serve } from 'https://deno.land/std/http/server.ts';
-const s = serve({ port: 3000 });
-console.log('http://localhost:3000/');
-for await (const req of s) {
-	req.respond({ body: 'Hello , Thank You' });
+import { Application, Router } from 'https://deno.land/x/oak@v6.4.0/mod.ts';
+
+const app = new Application();
+const router = new Router();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+interface Book {
+	id: String;
+	title: String;
+	author: string;
 }
+
+let books: Book[] = [
+	{
+		id: '1',
+		title: 'Book One',
+		author: 'One'
+	},
+	{
+		id: '2',
+		title: 'Book Two',
+		author: 'Two'
+	},
+	{
+		id: '1',
+		title: 'Book Three',
+		author: 'Three'
+	}
+];
+
+router.get('/', (context) => {
+	context.response.body = 'Hello WOrld!';
+});
+console.log('Server is listening on port 3000');
+await app.listen({ port: 3000 });
